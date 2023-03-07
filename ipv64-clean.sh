d454bfe08417039424dd6d8bb73e004423941f44
Roga Hagen <roga@hagen.me>
2023-03-07 12:11:42 +0100

    Update ipv64-clean.sh

--- ipv64-clean.sh
+++ ipv64-clean.sh
@@ -4,5 +4,6 @@ APIKEY="0123456789ABCDEFGHIJKLM"
 DOMAIN=$CERTBOT_DOMAIN
 PRAEFIX="_acme-challenge"
 TYPE="TXT"
+CONTENT=$CERTBOT_VALIDATION
 
-curl -s -X DELETE "https://ipv64.net/api.php" -H "Authorization: Bearer $APIKEY" -d "del_record=$DOMAIN&praefix=$PRAEFIX&type=$TYPE&content=$CERTBOT_VALIDATION"
+curl -s -X DELETE "https://ipv64.net/api.php" -H "Authorization: Bearer $APIKEY" -d "del_record=$DOMAIN&praefix=$PRAEFIX&type=$TYPE&content=$CONTENT"
